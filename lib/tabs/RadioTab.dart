253179f6bfda624570cf6361255f04709860546c
HatemReda7 <hreda2567@gmail.com>
2023-09-15 21:17:13 +0300

    created radio tab and added back icon, next icon, play icon

--- lib/tabs/RadioTab.dart
+++ lib/tabs/RadioTab.dart
@@ -5,8 +5,24 @@ class RadioTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset("assets/images/radio_image.png"),
+          SizedBox(height: 80,),
+          Text("إذاعة القرآن الكريم", style: Theme.of(context).textTheme.bodySmall!.copyWith(fontSize: 25),),
+          SizedBox(height: 50,),
+          Row(
+            children: [
+              ImageIcon(AssetImage("assets/images/icon metro-back.png")),
+              ImageIcon(AssetImage("assets/images/icon awesome-play.png")),
+              ImageIcon(AssetImage("assets/images/icon metro-next.png")),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
